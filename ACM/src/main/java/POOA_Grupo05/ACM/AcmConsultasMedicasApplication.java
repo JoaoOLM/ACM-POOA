0fd01c1b3b85202d6c85aa7d3b508ebeb473eaea
Vitorrecoaro <vitordearec@gmail.com>
2024-08-07 09:09:15 -0300

    feat: spring boot project
    Uploading spring boot project folder, to change projet from pure
    maven to maven with spring boot.

---
+++ ACM/src/main/java/POOA_Grupo05/ACM/AcmConsultasMedicasApplication.java
@@ -0,0 +1,13 @@
+package POOA_Grupo05.ACM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmConsultasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmConsultasMedicasApplication.class, args);
+	}
+
+}
---
+++ ACM/src/test/java/POOA_Grupo05/ACM/AcmConsultasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package POOA_Grupo05.ACM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmConsultasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
